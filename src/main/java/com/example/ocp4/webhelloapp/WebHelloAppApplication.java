d569580a90a94d36e87864e760d438ca5233d171
David Yang <david.yang@oracle.com>
2020-10-23 21:18:38 +0800

    first commit

---
+++ src/main/java/com/example/ocp4/webhelloapp/WebHelloAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ocp4.webhelloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHelloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebHelloAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ocp4/webhelloapp/WebHelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocp4.webhelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
