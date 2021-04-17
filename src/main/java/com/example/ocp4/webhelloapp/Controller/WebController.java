06b241cc80aa4697afc162e7c1effd667087b4d8
David <dyangcht@aim.com>
2021-04-17 16:14:30 +0800

    update output string

--- src/main/java/com/example/ocp4/webhelloapp/Controller/WebController.java
+++ src/main/java/com/example/ocp4/webhelloapp/Controller/WebController.java
@@ -11,16 +11,19 @@ public class WebController {
     @RequestMapping("/hello/v1/{name}")
     // @GetMapping("/{name}")
     public String hello(@PathVariable String name) {
+        System.out.println("hello");
         return "Hello 你好, " + name + "\n";
     }
 
     @RequestMapping("/prod/v1/{name}")
     public String prod(@PathVariable String name) {
+        System.out.println("product");
         return "Product name is " + name + "\n";
     }
 
     @RequestMapping("/query/v1/{name}")
     public String query(@PathVariable String name) {
+        System.out.println("query");
         return "Query string is " + name + "\n";
     }
 }
