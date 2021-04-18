2c4240e6926193befd6538ab2952d569c54387c0
David <dyangcht@aim.com>
2021-04-18 11:51:17 +0800

    update post

--- src/main/java/com/example/ocp4/webhelloapp/Controller/WebController.java
+++ src/main/java/com/example/ocp4/webhelloapp/Controller/WebController.java
@@ -1,9 +1,6 @@
 package com.example.ocp4.webhelloapp.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
